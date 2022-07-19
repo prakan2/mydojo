eca1db75302d6485e8b99ffe58701b1ebda9834a
friedrich <fvj@jfrog.com>
2022-07-19 17:54:27 +0300

    next

--- jpd-config/variables_example.tf
+++ jpd-config/variables_example.tf
@@ -7,3 +7,8 @@ variable "artifactory_access_token_expl" {
   type = string
   default = "cmeXamPlejAxOjE2ODc1MjkwNjI6dTJhZ29BM042eUg5WDJsWlFPUDMwYW13ZzlC"
 }
+
+variable "developer_pw" {
+  type = string
+  default = "MyV3ry$3cr3tP@$$w0rd"
+}
