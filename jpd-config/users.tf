4a67cbf198f78013b1675a7a4c721db27858a9b6
friedrich <fvj@jfrog.com>
2022-07-19 13:54:28 +0300

    next

--- jpd-config/users.tf
+++ jpd-config/users.tf
@@ -1,6 +1,6 @@
 resource "artifactory_user" "dojo-developer" {
-  name     = "coderking"
-  email    = "coderking@fritzdata.de"
+  name     = "dojodev"
+  email    = "dojodev@fritzdata.de"
   password = var.developer_pw
   disable_ui_access = false
   groups   = ["${artifactory_group.dojo-docker-developers.name}"]
